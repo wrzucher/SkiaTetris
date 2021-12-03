1a44494c55432f39188dfdb1d8663d6eb7d098d9
maarraa <maarraa@mail.ru>
2021-12-03 10:02:40 +0600

    Initial commit - Base tetris game - gitignore - Project configuration

---
+++ SkiaTetris/KeyboardEventGateway.cs
@@ -0,0 +1,34 @@
+﻿namespace SkiaTetris;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.JSInterop;
+
+public sealed class KeyboardEventGateway : IDisposable
+{
+    private static event Action<KeyboardEventArgs> KeyDownInternal;
+    public event Action<KeyboardEventArgs> OnKeyDown;
+
+    public KeyboardEventGateway()
+    {
+        KeyDownInternal += KeyboardEventGateway_KeyDownInternal;
+    }
+
+    public void Dispose()
+    {
+        KeyDownInternal -= KeyboardEventGateway_KeyDownInternal;
+    }
+
+    [JSInvokable("GlobalKeyDown")]
+    public static Task HandleKeyDown(KeyboardEventArgs e)
+    {
+        KeyDownInternal?.Invoke(e);
+        return Task.CompletedTask;
+    }
+
+    private void KeyboardEventGateway_KeyDownInternal(KeyboardEventArgs obj)
+    {
+        this.OnKeyDown?.Invoke(obj);
+    }
+}
---
+++ SkiaTetris/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SkiaTetris;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddSingleton<KeyboardEventGateway>();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
